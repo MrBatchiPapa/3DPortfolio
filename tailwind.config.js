7c53e052e21cbdaf91f5de73035b3fe6d3f484d4
Kevin Jin <jin@Kevins-MacBook-Pro.local>
2024-04-01 16:19:27 +1300

    Initial routing

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// tutorail uses .cjs extension, perhaps i need to change to that later
