acd2bd3e7533720b0f8b297cffff66a4320a74e8
Kevin Jin <jin@Kevins-MacBook-Pro.local>
2024-04-02 22:06:40 +1300

    hero section complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+// import React from "react";
+// import Tilt from "react-tilt";
+// import { motion } from "framer-motion";
 
+// import { styles } from "../styles";
+// import { services } from "../constants";
+// import { SectionWrapper } from "../hoc";
+// import { fadeIn, textVariant } from "../utils/motion";
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  return <>{/* <motion.div></motion.div> */}</>;
+};
 
-export default About
+export default About;
